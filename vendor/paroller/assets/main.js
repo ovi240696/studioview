542e91372e73e2b2752e28263860bd6a2a148107
Owais Khan <ovi240696@gmail.com>
2021-11-12 17:28:55 +0500

    form link added

--- vendor/paroller/assets/main.js
+++ vendor/paroller/assets/main.js
@@ -1,43 +1,53 @@
 (function ($) {
-    $('body').addClass('is-loading');
-    var menuSelector = $('#sidebar ul li a');
+    $("body").addClass("is-loading");
+    var menuSelector = $("#sidebar ul li a");
 
     $(document)
         .ready(function () {
-            if(window.location.hash) {
+            if (window.location.hash) {
                 $("html, body")
-                    .removeClass('is-loading')
-                    .animate({ scrollTop: $(window.location.hash).scrollTop() }, 1);
+                    .removeClass("is-loading")
+                    .animate(
+                        { scrollTop: $(window.location.hash).scrollTop() },
+                        1
+                    );
             } else {
                 setTimeout(function () {
                     $("html, body")
-                        .animate({scrollTop: $(document).height()}, 1)
-                        .animate({scrollTop: $("#download").scrollTop()}, 1);
-                    $('body').removeClass('is-loading').addClass('init-paroller');
+                        .animate({ scrollTop: $(document).height() }, 1)
+                        .animate({ scrollTop: $("#download").scrollTop() }, 1);
+                    $("body")
+                        .removeClass("is-loading")
+                        .addClass("init-paroller");
                 }, 1200);
             }
-            $('.show-more').on('click', function () {
-               $(this).toggleClass('active');
-               if($(this).hasClass('active')) {
-                   $(this).next('.popover').slideDown();
-               } else {
-                   $(this).next('.popover').slideUp();
-               }
+            $(".show-more").on("click", function () {
+                $(this).toggleClass("active");
+                if ($(this).hasClass("active")) {
+                    $(this).next(".popover").slideDown();
+                } else {
+                    $(this).next(".popover").slideUp();
+                }
             });
-            $('a[href^="#"]').on('click', function (e) {
+            $('a[href^="#"]').on("click", function (e) {
                 e.preventDefault();
                 var target = this.hash;
                 $target = $(target);
 
-                $('html, body').animate({
-                    'scrollTop': $target.offset().top
-                }, 600, 'linear', function () {
-                    window.location.hash = target;
-                    $(document).on("scroll", initScroll(menuSelector));
-                });
+                $("html, body").animate(
+                    {
+                        scrollTop: $target.offset().top,
+                    },
+                    600,
+                    "linear",
+                    function () {
+                        window.location.hash = target;
+                        $(document).on("scroll", initScroll(menuSelector));
+                    }
+                );
             });
         })
-        .on('scroll', function () {
+        .on("scroll", function () {
             initScroll(menuSelector);
         });
 
@@ -47,14 +57,15 @@
         menuSelector.each(function () {
             var currLink = $(this);
             var refElement = $(currLink.attr("href"));
-            if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+            if (
+                refElement.position().top <= scrollPosition &&
+                refElement.position().top + refElement.height() > scrollPosition
+            ) {
                 menuSelector.removeClass("active");
                 currLink.addClass("active");
-            }
-            else {
+            } else {
                 currLink.removeClass("active");
             }
         });
     }
-
 })(jQuery);
